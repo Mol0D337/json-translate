4664f970c47dc3fcf876893f29502a6c11830c85
Mol0D <moloduk99@gmail.com>
2021-06-15 12:27:43 +0300

    1

---
+++ dist/precache-manifest.5b79f6de364ac8347bdd250cab4b981f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6236992a7a7d3bfa90",
+    "url": "/css/app.eca8e225.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "79cd6f6fc777a4ab8e66db285c4329ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d424bd1980ffac76beaa",
+    "url": "/js/about.888c0ad4.js"
+  },
+  {
+    "revision": "8e6236992a7a7d3bfa90",
+    "url": "/js/app.375441dd.js"
+  },
+  {
+    "revision": "ee167c7e42f695339fd4",
+    "url": "/js/chunk-vendors.875086c4.js"
+  },
+  {
+    "revision": "6c19cbe25bb051a4908b9d674f103790",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
